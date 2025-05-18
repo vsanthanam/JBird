fe341559cedd5538a24e462e15169a0d5a98da28
Varun Santhanam <talkto@vsanthanam.com>
2025-05-18 10:31:45 -0700

    Rename `JBirdCore` to `JBirdParser` (#14)

--- Sources/JBird/Serialization/Deserializer.swift
+++ Sources/JBird/Serialization/Deserializer.swift
@@ -24,7 +24,7 @@
 // SOFTWARE.
 
 import Foundation
-import JBirdCore
+import JBirdParser
 
 @available(macOS 13.0, macCatalyst 16.0, iOS 16.0, watchOS 9.0, tvOS 16.0, visionOS 1.0, *)
 extension JSON {
