b8a4d5a8d6a4e68b2682a466df5b216242862b91
Varun Santhanam <talkto@vsanthanam.com>
2025-05-24 15:15:17 -0700

    Fix typo in result builder documentation (#65)

--- JBird/Builders/ObjectBuilder.swift
+++ JBird/Builders/ObjectBuilder.swift
@@ -51,7 +51,7 @@
 @available(macOS 13.0, macCatalyst 16.0, iOS 16.0, watchOS 9.0, tvOS 16.0, visionOS 1.0, *)
 extension JSON {
 
-    /// A result builder for JSON [String: JSON]s
+    /// A result builder for JSON objects
     @resultBuilder
     public enum ObjectBuilder {
 
