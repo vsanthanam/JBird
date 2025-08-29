a5fd4c47834d82e1980ff7b1606e5b164ed16118
Varun Santhanam <talkto@vsanthanam.com>
2025-08-28 17:00:12 -0700

    Upgrate to SwiftFormat 0.57.2 (#127)

--- Package.swift
+++ Package.swift
@@ -50,7 +50,7 @@ let package = Package(
         ),
         .package(
             url: "https://github.com/nicklockwood/SwiftFormat.git",
-            exact: "0.56.4"
+            exact: "0.57.2"
         )
     ],
     targets: [
