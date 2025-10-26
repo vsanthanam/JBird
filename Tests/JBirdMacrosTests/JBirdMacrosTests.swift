c68c046ced9aaa7ab75f090b73c002cd47b8e5e4
Varun Santhanam <talkto@vsanthanam.com>
2025-10-25 19:39:19 -0700

    Add macro end-to-end unit tests

--- Tests/JBirdMacrosTests/JBirdMacrosTests.swift
+++ Tests/JBirdMacrosTests/JBirdMacrosTests.swift
@@ -29,141 +29,176 @@ import JBirdCore
 import JBirdMacros
 import Testing
 
-@JSONCodable
-private struct Foo: Equatable {
+@Suite("@JSONCodable Tests")
+struct JSONCodableTests {
 
-    init(
-        fooBar: Int?,
-        nested: TestNested?,
-        id: String,
-        optionalWithoutAnnotation: Int?,
-        nilIfMissing: Double?
-    ) {
-        self.fooBar = fooBar
-        self.nested = nested
-        self.id = id
-        self.optionalWithoutAnnotation = optionalWithoutAnnotation
-        self.nilIfMissing = nilIfMissing
+    @JSONCodable
+    struct Foo: Equatable {
+
+        init(
+            fooBar: Int?,
+            nested: TestNested?,
+            id: String,
+            optionalWithoutAnnotation: Int?,
+            nilIfMissing: Double?
+        ) {
+            self.fooBar = fooBar
+            self.nested = nested
+            self.id = id
+            self.optionalWithoutAnnotation = optionalWithoutAnnotation
+            self.nilIfMissing = nilIfMissing
+        }
+
+        @JSONKey(.snakeCase)
+        let fooBar: Int?
+
+        @OmitIfNil
+        let nested: TestNested?
+
+        let id: String
+
+        let optionalWithoutAnnotation: Int?
+
+        @OmitIfNil(false)
+        let nilIfMissing: Double?
+
     }
 
-    @JSONKey(.snakeCase)
-    let fooBar: Int?
+    @JSONCodable
+    struct TestNested: Equatable {
 
-    @OmitIfNil
-    let nested: TestNested?
+        init(name: String) {
+            self.name = name
+        }
 
-    let id: String
+        let name: String
 
-    let optionalWithoutAnnotation: Int?
-
-    @OmitIfNil(false)
-    let nilIfMissing: Double?
-
-}
-
-@JSONCodable
-struct TestNested: Equatable {
-
-    init(name: String) {
-        self.name = name
     }
 
-    let name: String
+    @JSONCodable
+    enum TestEnum: Equatable {
 
-}
+        case foo, bar
+        case baz(Int)
+        case qux(String, String?)
+        case quux(foo: Int, Double)
+        case corge(String, Int?)
+        case grault(foo: String?, bar: String)
+        case taco([Int])
+        case burrito([String: String])
 
-@JSONCodable
-enum TestEnum: Equatable {
+    }
 
-    case foo, bar
-    case baz(Int)
-    case qux(String, String?)
-    case quux(foo: Int, Double)
-    case corge(String, Int?)
-    case grault(foo: String?, bar: String)
-    case taco([Int])
-    case burrito([String: String])
+    @JSONCodable
+    class TestClass {
 
-}
+        init(foo: Int?, bar: String) {
+            self.foo = foo
+            self.bar = bar
+        }
 
-@Test("Test @JSONCodable Sample")
-func jsonCodableSample() throws {
+        @OmitIfNil(false)
+        let foo: Int?
 
-    let model = Foo(
-        fooBar: 12,
-        nested: nil,
-        id: "123",
-        optionalWithoutAnnotation: nil,
-        nilIfMissing: nil
-    )
-    let json = JSON(model)
-    let decoded = try Foo(json: json)
-    #expect(model == decoded)
-    #expect(json == [
-        "foo_bar": 12,
-        "id": "123",
-        "nilIfMissing": .null
-    ])
+        let bar: String
 
-}
+    }
 
-@Suite("@JSONCodable Enum Support")
-struct EnumSupport {
+    final class TestSubClass: TestClass {}
 
-    @Test("Basic enum decode")
-    func basic() throws {
-        let model = TestEnum.foo
+    @Test("@JSONCodable Class Support")
+    func classSupport() throws {
+        let model = TestSubClass(foo: nil, bar: "foo")
         let json = JSON(model)
-        let decoded = try TestEnum(json: json)
-        #expect(model == decoded)
-        #expect(json == "foo")
+        let decoded = try TestClass(json: json)
+        #expect(decoded.foo == model.foo)
+        #expect(decoded.bar == model.bar)
+        #expect(json == [
+            "foo": nil,
+            "bar": "foo"
+        ])
     }
 
-    @Test("Enum with unlabled value")
-    func singleUnlabledValue() throws {
-        let model = TestEnum.baz(12)
+    @Test("@JSONCodable Struct Support")
+    func structSupport() throws {
+
+        let model = Foo(
+            fooBar: 12,
+            nested: nil,
+            id: "123",
+            optionalWithoutAnnotation: nil,
+            nilIfMissing: nil
+        )
         let json = JSON(model)
-        let decoded = try TestEnum(json: json)
+        let decoded = try Foo(json: json)
         #expect(model == decoded)
-        #expect(json == ["baz": 12])
+        #expect(json == [
+            "foo_bar": 12,
+            "id": "123",
+            "nilIfMissing": .null
+        ])
+
     }
 
-    @Test("Enum with multiple unlabled values")
-    func multipleUnlabledValue() throws {
-        let model = TestEnum.corge("foo", nil)
-        let json = JSON(model)
-        let decoded = try TestEnum(json: json)
-        #expect(model == decoded)
-        #expect(json == ["corge": ["foo", nil]])
-    }
+    @Suite("@JSONCodable Enum Support")
+    struct EnumSupport {
 
-    @Test("Enum with multiple unlabled values of the same type")
-    func multipleUnlabledSameTypeValue() throws {
-        let model = TestEnum.qux("foo", "bar")
-        let json = JSON(model)
-        let decoded = try TestEnum(json: json)
-        #expect(model == decoded)
-        #expect(json == ["qux": ["foo", "bar"]])
-    }
+        @Test("Basic enum decode")
+        func basic() throws {
+            let model = TestEnum.foo
+            let json = JSON(model)
+            let decoded = try TestEnum(json: json)
+            #expect(model == decoded)
+            #expect(json == "foo")
+        }
 
-    @Test("Enum with mixed label associated values")
-    func mixedValues() throws {
-        let model = TestEnum.quux(foo: 12, 3.4)
-        let json = JSON(model)
-        print(json)
-        let decoded = try TestEnum(json: json)
-        #expect(model == decoded)
-        #expect(json == ["quux": ["foo": 12, "1": 3.4]])
-    }
+        @Test("Enum with unlabled value")
+        func singleUnlabledValue() throws {
+            let model = TestEnum.baz(12)
+            let json = JSON(model)
+            let decoded = try TestEnum(json: json)
+            #expect(model == decoded)
+            #expect(json == ["baz": 12])
+        }
 
-    @Test("Enum with multiple labeled associated values")
-    func labeledValues() throws {
-        let model = TestEnum.grault(foo: "bar", bar: "foo")
-        let json = JSON(model)
-        print(json)
-        let decoded = try TestEnum(json: json)
-        #expect(model == decoded)
-        #expect(json == ["grault": ["foo": "bar", "bar": "foo"]])
+        @Test("Enum with multiple unlabled values")
+        func multipleUnlabledValue() throws {
+            let model = TestEnum.corge("foo", nil)
+            let json = JSON(model)
+            let decoded = try TestEnum(json: json)
+            #expect(model == decoded)
+            #expect(json == ["corge": ["foo", nil]])
+        }
+
+        @Test("Enum with multiple unlabled values of the same type")
+        func multipleUnlabledSameTypeValue() throws {
+            let model = TestEnum.qux("foo", "bar")
+            let json = JSON(model)
+            let decoded = try TestEnum(json: json)
+            #expect(model == decoded)
+            #expect(json == ["qux": ["foo", "bar"]])
+        }
+
+        @Test("Enum with mixed label associated values")
+        func mixedValues() throws {
+            let model = TestEnum.quux(foo: 12, 3.4)
+            let json = JSON(model)
+            print(json)
+            let decoded = try TestEnum(json: json)
+            #expect(model == decoded)
+            #expect(json == ["quux": ["foo": 12, "1": 3.4]])
+        }
+
+        @Test("Enum with multiple labeled associated values")
+        func labeledValues() throws {
+            let model = TestEnum.grault(foo: "bar", bar: "foo")
+            let json = JSON(model)
+            print(json)
+            let decoded = try TestEnum(json: json)
+            #expect(model == decoded)
+            #expect(json == ["grault": ["foo": "bar", "bar": "foo"]])
+        }
+
     }
 
 }
