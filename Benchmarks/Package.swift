9bac0a7d58cff7300bc67d119943cc5256a7d020
Varun Santhanam <talkto@vsanthanam.com>
2026-01-17 09:26:48 -0800

    Update to version 2.0.3

--- Benchmarks/Package.swift
+++ Benchmarks/Package.swift
@@ -29,7 +29,7 @@ let jbird: PackageDescription.Package.Dependency = switch source {
 case .release:
     .package(
         url: "https://github.com/vsanthanam/JBird.git",
-        exact: "2.0.2"
+        exact: "2.0.3"
     )
 case .remote:
     .package(
