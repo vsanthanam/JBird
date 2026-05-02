9888abecb5c21df4c488e440ee92537c817f0ccf
Varun Santhanam <talkto@vsanthanam.com>
2026-05-02 15:14:13 -0700

    Update SwiftFormat to 0.61.1

--- Package.swift
+++ Package.swift
@@ -62,7 +62,7 @@ let package = Package(
         ),
         .package(
             url: "https://github.com/nicklockwood/SwiftFormat.git",
-            exact: "0.61.0"
+            exact: "0.61.1"
         )
     ],
     targets: [
