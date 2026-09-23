df1f342a5313dacf33e9f2a52911f133445a16ce
Varun Santhanam <talkto@vsanthanam.com>
2026-09-23 11:24:17 -0700

    Delete Deprecated Error Types (#424)

--- Sources/JBirdCore/Deserialization/DeserializationError.swift
+++ Sources/JBirdCore/Deserialization/DeserializationError.swift
@@ -187,8 +187,3 @@ extension JSON {
     }
 
 }
-
-/// A deprecated alias for ``JSON/DeserializationError``.
-@available(macOS 13.0, macCatalyst 16.0, iOS 16.0, watchOS 9.0, tvOS 16.0, visionOS 1.0, *)
-@available(*, deprecated, renamed: "JSON.DeserializationError")
-public typealias JSONDeserializationError = JSON.DeserializationError
